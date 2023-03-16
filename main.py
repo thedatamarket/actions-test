6939218e018701caacfac045fc11a303aef8a9e5
The Data Market <110961258+thedatamarket@users.noreply.github.com>
2023-03-16 12:07:37 +0530

    Update main.py
    Changed the theme to twitter trends

--- main.py
+++ main.py
@@ -1,38 +1,89 @@
+from twython import Twython
+import tweepy
 import requests
-import yfinance as yf
+import time
+from datetime import datetime
+import pandas as pd
+import pytz
+import logging
+import logging.handlers
+
 def msg(message):
     print(message)
-    news = message.replace("(","").replace(")","").replace(".",",").replace("-","/")
+    
     bot_token = '5041715929:AAFcraPI9-8jZR0bLkquRDNUXg96tEUKje4'
     bot_chatID = '1259144189'
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id='+ bot_chatID + '&parse_mode=MarkdownV2&text=' + news
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id='+ bot_chatID + '&parse_mode=MarkdownV2&text=' + message
 
     response = requests.get(send_text)
     print(response.json())
     return response.json()
 
-def get_current_price(symbol):
-    ticker = yf.Ticker(symbol)
-    todays_data = ticker.history(period='1d')
-    return todays_data['Close'][0]
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s, %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
 
-ticker = ['HINDUNILVR.NS','RECLTD.NS','JPPOWER.NS']
+# consumer_key = "gr4e3Si8YqjiTcsERE0X1KgG7"
+# consumer_secret = "gs9rbOZfVl9pBxoXXykSfhz2x5Dugf5BoocI7dUjf3n8O6ytrZ"
+# access_token = "1392839370843906050-gbehapjFj9Mbb3hxIm8qesHrnO4mmu"
+# access_token_secret = "UgDjETJODHOb16DNFQWnuJ1B96UNPKUguakNFsHjuuOzn"
 
-msg("WatchList")
-for i in ticker:
-    txt = str(i) + ' ' + str(get_current_price(i))
-    msg(txt)
+consumer_key = "Kk6YsDOoMrZ3dleewI5PyoFBG"
+consumer_secret = "SfNeq91UGvi7Se797iEflaOsuQ3M61nfJey04PTc3Oikz9FvBm"
+access_token = "1392839370843906050-hWAZNtev8MaisB2xdv3hPBQ7ybkm8r"
+access_token_secret = "ReY9BzjN82rHAApkuTi7j7cIYxv4YxVIYHimg6Gwd1jQ2"
+start = 0
+try:
+    if start == 0:
+        twitter = Twython(
+            consumer_key,
+            consumer_secret,
+            access_token,
+            access_token_secret
+        )
+        # authorization of consumer key and consumer secret
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        
+        # set access to user's access key and access secret 
+        auth.set_access_token(access_token, access_token_secret)
+        
+        # calling the api 
+        api = tweepy.API(auth)
+        
+        # WOEID of London
+        woeid_ind = 23424848
+        
+        # fetching the trends
+        trends = api.trends_place(id = woeid_ind)
+        
+        # printing the information
+        print("The top trends for the location are :")
+        trendLst = []
 
-msg("Portfolio")
-port = {
-    "TCS.NS" : 2,
-    "WIPRO.NS" : 2,
-    "INFY.NS" : 1,
-    "NTPC.NS" : 5,
-    "HINDUNILVR.NS":3
-}
-sum = 0
-for i in port:
-    sum = int(int(get_current_price(i))*port[i]) + sum 
-txt = "Portfolio " + str(sum) + " of 17795"
-msg(str(txt))
+        print("The top trends for the location are :")
+        trendLst = []
+        for value in trends:
+            for trend in value['trends']:
+                trendLst.append(trend['name'])
+                now = datetime.now()
+                current_time = datetime.now(pytz.timezone('Asia/Kolkata'))
+                print(current_time.date(),current_time.time(),trend['name'],trend['tweet_volume'])
+                c_date = str(current_time.date())
+                c_time = current_time.time()
+                trending = trend['name']
+                volume = trend['tweet_volume']
+
+                logger.info(f'{c_date},{c_time},{trending},{volume}')
+except Exception as e:
+  
+  msg("Process Stopped Need your attention")
+
+
